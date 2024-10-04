57424a25a342397fd5c57901ca21d5e14253b6b3
TISEPSE <titi21player@gmail.com>
2024-10-04 15:40:57 +0200

    responsive confetti

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ const correctIndexes = {
 let btnValider = document.getElementById("btnValider") // Bouton qui vérifie si tout est correct
 
 btnValider.addEventListener("click", () => {
+  let allCorrect = true // Cette variable vérifie si tout est correct
+
   for (let i = 1; i <= 7; i++) {
     let select = document.querySelector(`.select${i}`)
     // Enlever les classes "green" et "red" à chaque changement d'option
@@ -33,24 +35,51 @@ btnValider.addEventListener("click", () => {
     } else {
       console.log("Le pays n'est pas correct")
       select.classList.add("red")
+      allCorrect = true // Si une option est incorrecte, tout n'est pas correct
     }
   }
-})
 
-let btnReset = document.getElementById("reset")
-btnReset.addEventListener("click", () => {
-  for (let i = 1; i <= 7; i++) {
-    // Enlève les classes "green" et "red" de tous les selects
-    let select = document.querySelector(`.select${i}`)
-    select.classList.remove("green", "red")
+  let btnReset = document.getElementById("reset")
+  btnReset.addEventListener("click", () => {
+    for (let i = 1; i <= 7; i++) {
+      // Enlève les classes "green" et "red" de tous les selects
+      let select = document.querySelector(`.select${i}`)
+      select.classList.remove("green", "red")
+    }
+  })
+
+  // Si toutes les sélections sont correctes, on affiche les confettis
+  if (allCorrect) {
+    const duration = 5 * 900,
+      animationEnd = Date.now() + duration,
+      defaults = {startVelocity: 30, spread: 360, ticks: 60, zIndex: 0}
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min
+    }
+
+    const interval = setInterval(function () {
+      const timeLeft = animationEnd - Date.now()
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval)
+      }
+
+      const particleCount = 50 * (timeLeft / duration)
+
+      // since particles fall down, start a bit higher than random
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2},
+        })
+      )
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2},
+        })
+      )
+    }, 250)
   }
 })
-
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
-confetti();
-
-function party(){
-  confetti()
-}
-
-document.getElementById('confetti').addEventListener('click', party)
